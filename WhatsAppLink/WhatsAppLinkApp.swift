a760f777e4ad7ddd0800c8fe1fd79eb71f49acb5
agmcoder <agmcoder@gmail.com>
2022-10-26 10:52:29 +0200

    Initial Commit

---
+++ WhatsAppLink/WhatsAppLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppLinkApp.swift
+//  WhatsAppLink
+//
+//  Created by agmcoder on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
